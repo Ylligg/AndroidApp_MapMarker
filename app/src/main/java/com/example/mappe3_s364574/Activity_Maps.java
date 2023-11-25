f73d37f80b44c950d94d3a996fba4ce36eb8800d
ylli <s364574@oslomet.no>
2023-11-25 16:14:05 +0100

    fixed comments and strings.xml

--- app/src/main/java/com/example/mappe3_s364574/Activity_Maps.java
+++ app/src/main/java/com/example/mappe3_s364574/Activity_Maps.java
@@ -1,18 +1,14 @@
 package com.example.mappe3_s364574;
 
 import androidx.annotation.NonNull;
-
 import androidx.fragment.app.FragmentActivity;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.example.mappe3_s364574.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,17 +16,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
-
 import javax.net.ssl.HttpsURLConnection;
 
 public class Activity_Maps extends FragmentActivity implements OnMapReadyCallback {
@@ -49,11 +42,12 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // gjør kartet klart for å brukes
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        // gjør et kall til jsonin filen for å hente dataene
         getJSON task = new getJSON();
         task.execute("https://dave3600.cs.oslomet.no/~s364574/jsonout.php");
 
@@ -70,7 +64,7 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
             String s = "";
             String output = "";
             for (String url : urls) {
-
+                // prøver å lage en tilkobling til siden
                 try {
                     URL urlen = new URL(urls[0]);
                     HttpsURLConnection conn = (HttpsURLConnection)
@@ -79,10 +73,13 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
                     conn.setRequestProperty("Accept","application/json");
                     conn.connect();
 
+                    // om den ikke ble koblet til så gis det en melding
                     if (conn.getResponseCode() != 200) {
                         throw new RuntimeException("Failed : HTTP error code :"+
                                 conn.getResponseCode());
                     }
+
+                    // hvis det gikk bra så vil datanee bli lagt til i output og steder variablene
                     BufferedReader br = new BufferedReader(new InputStreamReader(
                             (conn.getInputStream())));
                     System.out.println("Output from Server .... \n");
@@ -92,6 +89,7 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
                     }
                     conn.disconnect();
 
+                    // nå så leser vi jsondataene og omgjør de til vanlig strings og får all infoen vi trenger
                     try { JSONArray mat = new JSONArray(output);
                         for (int i = 0; i < mat.length(); i++) {
                             JSONObject jsonobject = mat.getJSONObject(i);
@@ -101,6 +99,7 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
                             String cLAT = jsonobject.getString("CoordsLAT");
                             String cLONG = jsonobject.getString("CoordsLONG");
 
+                            // lager en string med all oversatt data som skal sendes til siden
                             retur = retur + "Place: "+ name +" Description: "+ des +" Address: "+ adress +" Latitude: "+ cLAT +" Longitude: "+ cLONG + "\n\n";
                         }
                         return retur;
@@ -112,6 +111,7 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
                     return "Noe gikk feil" + e;
                 }
             }
+            // returnerer dataene
             return retur;
         }
         @Override
@@ -121,17 +121,17 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
     }
 
     //gjør kartet klar med markørere fra jsonobjektet
-
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
-
+        // venter til at kartet er klar for bruk slik at dataene fra json tjenesten kan bli gjort om til markører
         if(mMap!=null) {
 
+        // karett loader ferdig og så vil markørene bli opprettet
         mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
             @Override
             public void onMapLoaded() {
-
+                // har en try for å få alle datane fra Jsonout.php
                 try{
                     JSONArray mat = new JSONArray(steder);
                     for (int i = 0; i < mat.length(); i++) {
@@ -142,6 +142,8 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
                         double cLAT = jsonobject.getDouble("CoordsLAT");
                         double cLONG = jsonobject.getDouble("CoordsLONG");
 
+                        //oppretter LatLng kordinater som blir brukt for markører som har tittel og beskrivelse
+                        // så når vi går inn i kartet så er alle markører opprettet
                         LatLng coordinater = new LatLng(cLAT, cLONG);
                         mMap.addMarker(new MarkerOptions().position(coordinater).title("Marker in " + name).snippet("Description: " + des + " Sted: " + adress));
 
@@ -153,60 +155,76 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
             }
         });
 
+        // når alle markører er lagd så kan vi lage nye markører ved å trykke hvor som helst på kartet
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(@NonNull LatLng latLng) {
 
-                Toast.makeText(Activity_Maps.this, "CLICK ON MAP" + latLng, Toast.LENGTH_SHORT).show();
+                // lager et input felt med sted,beskrivelse og addresse som skal brukes for vår alert
                 LayoutInflater layoutInflater = getLayoutInflater();
                 View view = layoutInflater.inflate(R.layout.custom_alert, null);
 
+                // initialiserer edittextene
                 sted = (EditText) view.findViewById(R.id.navnsted);
                 beskrivelse = (EditText) view.findViewById(R.id.beskrivelse);
                 addresse = (EditText) view.findViewById(R.id.addresse);
 
+                // strings fra strings.xml
+                String tittel = getResources().getString(R.string.nymarkør);
+
+                String ja = getResources().getString(R.string.ja);
+                String nei = getResources().getString(R.string.nei);
+
+                // lager en alertdialog
                 new AlertDialog.Builder(Activity_Maps.this)
-                        .setMessage("Ny Markør?")
-                        .setCancelable(false)
-                        .setView(view)
-                        .setPositiveButton("ja", new DialogInterface.OnClickListener() {
-                            @SuppressLint("ResourceType")
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                String stedtxt = sted.getText().toString();
-                                String beskrivelsetxt = beskrivelse.getText().toString();
-                                String addressetxt = addresse.getText().toString();
-                                mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in " + stedtxt).snippet("Description: " + beskrivelsetxt + " Sted: " + addressetxt));
-                                System.out.println(latLng + " : " + latLng.latitude);
-                                postJSON("https://dave3600.cs.oslomet.no/~s364574/jsonin.php?NamePlace=" + stedtxt + "&Description=" + beskrivelsetxt + "&Address=" + addressetxt + "&CoordsLAT=" +latLng.latitude + "&CoordsLONG=" + latLng.longitude);
+                    .setMessage(tittel)
+                    .setCancelable(false)
+                    .setView(view)
 
-                            }
-                        })
-                        .setNegativeButton("nei", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
+                        // Når man trykker ja så vil datane fra inputfeltet bli tatt
+                        // og bli lagt til en ny addMarker så kan vi se på kartet at det ble opprettet en markør
+                        // etter at markøren er der så vil vi at den skal være der når appen startes på nytt
+                        // så vi kaller på postJSON for å ta datene og lagre det i jsonin.php slik at markøren blir lagret
+                        // slik at neste gang så kan vi fortsatt se den og interacte med den
+                    .setPositiveButton(ja, new DialogInterface.OnClickListener() {
+                        @SuppressLint("ResourceType")
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            String stedtxt = sted.getText().toString();
+                            String beskrivelsetxt = beskrivelse.getText().toString();
+                            String addressetxt = addresse.getText().toString();
+                            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in " + stedtxt).snippet("Description: " + beskrivelsetxt + " Sted: " + addressetxt));
+                            System.out.println(latLng + " : " + latLng.latitude);
+                            postJSON("https://dave3600.cs.oslomet.no/~s364574/jsonin.php?NamePlace=" + stedtxt + "&Description=" + beskrivelsetxt + "&Address=" + addressetxt + "&CoordsLAT=" +latLng.latitude + "&CoordsLONG=" + latLng.longitude);
 
-                            }
-                        })
-                        .show();
+                        }
+                    })
+                        // hvis nei er valgt så skjer ingenting
+                    .setNegativeButton(nei, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    })
+                    .show();
             }
         });
 
+        // når du trykker på markørern så vil det komme opp
+        // en toast av hva brukeren har trykket på (dette er ikke nødvendig men kan ha det)
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                // on marker click we are getting the title of our marker
-                // which is clicked and displaying it in a toast message.
+
                 String markerName = marker.getTitle();
                 Toast.makeText(Activity_Maps.this, "Clicked location is " + markerName, Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
-
+        }
     }
 
-    }
-
+    // metoden tar urlen om jsonin og executer den
     @SuppressLint("StaticFieldLeak")
     @SuppressWarnings("deprecation")
     private void postJSON(String url){
@@ -215,5 +233,4 @@ public class Activity_Maps extends FragmentActivity implements OnMapReadyCallbac
         post.execute(url);
 
     }
-
 }
--- app/src/main/java/com/example/mappe3_s364574/MainActivity.java
+++ app/src/main/java/com/example/mappe3_s364574/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.mappe3_s364574;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -10,23 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.example.mappe3_s364574.databinding.ActivityMapsBinding;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-
 import javax.net.ssl.HttpsURLConnection;
 @SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
@@ -36,20 +23,27 @@ public class MainActivity extends AppCompatActivity {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_main);
 
+                // har en textview for å displaye dataene til jsonout.php
                 textView = (TextView) findViewById(R.id.jasontekst);
+                // sender request til jsonout.php
+
                 getJSON task = new getJSON();
                 task.execute("https://dave3600.cs.oslomet.no/~s364574/jsonout.php");
 
+                // har en knapp for å gå til kart med markører
                 Button button = findViewById(R.id.button);
+
+                // når man trykker knappen så vil man gå til kartet
                 button.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                                 Intent intent = new Intent(MainActivity.this, Activity_Maps.class);
                                 startActivity(intent);
-
                         }
                 });
         }
+
+        // samme funksjon som i activity_maps se kommentarer der
         @SuppressLint("StaticFieldLeak")
         @SuppressWarnings("deprecation")
         private class getJSON extends AsyncTask<String, Void,String> {
